91d9ffdd38ed0c2f79e2935c31a7889f19a36f6f
René Martínez <hola@r3n0.com>
2022-12-15 11:38:11 -0500

    ingreso y estructura de datos

--- sketch.js
+++ sketch.js
@@ -1,7 +1,76 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+const celdas = [];
+const RETICULA = 5;
+
+const azulejos = [];
+const NA = 6; // número de azulejos
+
+const reglas = [
+	// reglas de los borded de cada azulejo
+	{
+		// tile 0
+		UP: 0,
+		RIGHT: 0,
+		DOWN: 0,
+		LEFT: 0,
+	},
+	{
+		// tile 1
+		UP: 1,
+		RIGHT: 1,
+		DOWN: 1,
+		LEFT: 0,
+	},
+	{
+		// tile 2
+		UP: 0,
+		RIGHT: 1,
+		DOWN: 1,
+		LEFT: 1,
+	},
+	{
+		// tile 3
+		UP: 1,
+		RIGHT: 1,
+		DOWN: 0,
+		LEFT: 1,
+	},
+	{
+		// tile 4
+		UP: 1,
+		RIGHT: 0,
+		DOWN: 1,
+		LEFT: 1,
+	},
+	{
+		// tile 5
+		UP: 1,
+		RIGHT: 0,
+		DOWN: 0,
+		LEFT: 1,
+	},
+];
+
+function preload() {
+	for (let i = 0; i < NA; i++) {
+		azulejos[i] = loadImage(`azulejos/tile${i}.png`);
+	}
 }
 
-function draw() {
-  circle(mouseX, mouseY, 20);
+function setup() {
+	createCanvas(1080, 1080);
+
+	let opcionesI = [];
+	for (let i = 0; i < azulejos.length; i++) {
+		opcionesI.push(i);
+	}
+
+	for (let i = 0; i < RETICULA * RETICULA; i++) {
+		celdas[i] = {
+			colapsada: false,
+			opciones: opcionesI,
+		};
+	}
+	print(celdas);
 }
+
+function draw() {}
