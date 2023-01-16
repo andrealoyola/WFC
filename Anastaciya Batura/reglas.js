08fd16eff64ff1d21f490517dcc51cd25878eefb
René Martínez <hola@r3n0.com>
2023-01-16 15:03:07 -0500

    cambio de estructura para aceptar las propuestas de los estudiantes de ilustración de personajes

---
+++ Anastaciya Batura/reglas.js
@@ -0,0 +1,109 @@
+const NA = 15; // número de azulejos
+const reglas = [
+	// reglas de los borded de cada azulejo
+	{
+		// tile 0
+		UP: 0,
+		RIGHT: 0,
+		DOWN: 0,
+		LEFT: 0,
+	},
+	{
+		// tile 1
+		UP: 0,
+		RIGHT: 1,
+		DOWN: 0,
+		LEFT: 0,
+	},
+	{
+		// tile 2
+		UP: 0,
+		RIGHT: 0,
+		DOWN: 0,
+		LEFT: 2,
+	},
+	{
+		// tile 3
+		UP: 0,
+		RIGHT: 2,
+		DOWN: 0,
+		LEFT: 0,
+	},
+	{
+		// tile 4
+		UP: 0,
+		RIGHT: 0,
+		DOWN: 0,
+		LEFT: 1,
+	},
+	{
+		// tile 5
+		UP: 0,
+		RIGHT: 0,
+		DOWN: 1,
+		LEFT: 0,
+	},
+	{
+		// tile 6
+		UP: 0,
+		RIGHT: 0,
+		DOWN: 2,
+		LEFT: 0,
+	},
+	{
+		// tile 7
+		UP: 1,
+		RIGHT: 0,
+		DOWN: 0,
+		LEFT: 0,
+	},
+	{
+		// tile 8
+		UP: 2,
+		RIGHT: 0,
+		DOWN: 0,
+		LEFT: 0,
+	},
+	{
+		// tile 9
+		UP: 1,
+		RIGHT: 1,
+		DOWN: 1,
+		LEFT: 1,
+	},
+	{
+		// tile 10
+		UP: 2,
+		RIGHT: 2,
+		DOWN: 2,
+		LEFT: 2,
+	},
+	{
+		// tile 11
+		UP: 2,
+		RIGHT: 2,
+		DOWN: 2,
+		LEFT: 0,
+	},
+	{
+		// tile 12
+		UP: 1,
+		RIGHT: 1,
+		DOWN: 2,
+		LEFT: 1,
+	},
+	{
+		// tile 13
+		UP: 2,
+		RIGHT: 2,
+		DOWN: 1,
+		LEFT: 2,
+	},
+	{
+		// tile 14
+		UP: 1,
+		RIGHT: 0,
+		DOWN: 1,
+		LEFT: 1,
+	},
+];
